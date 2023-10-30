27b2ee42b885793ffeb756f03ac591bbdcfc66fc
AbbaAdam1 <sonicdx15@gmail.com>
2023-10-30 16:34:53 -0400

    update git ignore

--- .next/static/webpack/webpack.6cca4da2183d3f3c.hot-update.js
+++ .next/static/webpack/webpack.6cca4da2183d3f3c.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "edbb64e158b8b2b5"; }
+/******/ 	__webpack_require__.h = function() { return "def1ac725943f063"; }
 /******/ }();
 /******/ 
 /******/ }
