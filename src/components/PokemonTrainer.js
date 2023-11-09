13026b86adc2286e8dba4e7471cd586b48d8fc00
AbbaAdam1 <sonicdx15@gmail.com>
2023-11-08 21:18:09 -0500

    Updates for responsive design

--- src/components/PokemonTrainer.js
+++ src/components/PokemonTrainer.js
@@ -60,10 +60,10 @@ const PokemonTrainer = ({ user }) => {
           />
         </div>
 
-        <div className="grid grid-cols-4 sm:grid-cols-5 md:grid-cols-6 gap-9 sm:gap-0 mt-6 absolute top-20 left-0">
+        <div className="grid grid-cols-4 sm:grid-cols-5 md:grid-cols-6 mt-6 absolute top-20 left-0">
           {userPokemon.map((pokemonData, index) => (
             <div key={index} className="cursor-pointer">
-              <div className="relative w-30 h-10 sm:w-24 sm:h-24 md:w-36 md:h-36">
+              <div className="relative w-30 h-20 sm:w-24 sm:h-24 md:w-36 md:h-36">
                 <img
                   src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`}
                   alt={pokemonData.name}
