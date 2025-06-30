0b2eb6a0be37575eea312ff7ade8b8ebd324c694
AbbaAdam1 <sonicdx15@gmail.com>
2025-06-30 06:55:05 -0400

    testing

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -17,9 +17,6 @@ export default async function Profile() {
     redirect('/sign-in');
   }
 
-  //      <div className="heading">Last Signed In:</div>
-  //      <code className="highlight">{new Date(user.last_sign_in_at).toUTCString()}</code>
-
   return (
     <div className="card">
       <p className="text-2xl font-bold">User Profile</p>
